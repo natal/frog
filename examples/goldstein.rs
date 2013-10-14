ee9e79788523f6eee069dccb68bc334e8608c0dc
roux_b <stuart.benji@gmail.com>
2013-10-14 11:23:38 +0200

    Fix goldstein example

--- examples/goldstein.rs
+++ examples/goldstein.rs
@@ -9,6 +9,11 @@ fn bench(name: &str, thresh: f64, ga: &mut GeneticAlgorithm<f64>, fit_fn: &fn(co
 {
     let old = time::precise_time_s();
     println("Testing " + name +" function\n");
+
+    let mut test_arr: ~[f64] = ~[];
+    test_arr.push(0.5);
+    test_arr.push(0.25);
+    println(fit_fn(test_arr).to_str());
     let mut count = 0u;
     while ga.best.score <= thresh {
       do ga.step |c| {
@@ -33,7 +38,7 @@ fn bench(name: &str, thresh: f64, ga: &mut GeneticAlgorithm<f64>, fit_fn: &fn(co
 fn main() {
     let mut ga: GeneticAlgorithm<f64> = GeneticAlgorithm::new(3000, 2, 0.8, 60.0, 100);
     let bmax = 2.0 * 2.0;
-    do bench("Goldstein", -3.0, &mut ga) |c| {
+    do bench("Goldstein", -3.0001, &mut ga) |c| {
         // Restrict to hypercube -5.12 <= x <= 5.12
         let e1 = (c[0] - 0.5) * bmax;
         let e2 = (c[1] - 0.5) * bmax;
@@ -42,7 +47,7 @@ fn main() {
                  (19.0 - 14.0 * e1 + 13.0 * e1.pow(~2.0) - 14.0 * e2 +
                   6.0 * e1 * e2 + 3.0 * e2.pow(~2.0));
         let b =  30.0 + (2.0 * e1 - 3.0 * e2).pow(~2.0) *
-                 (18.0 - 32.0 * e1 + 12.0 * e1.pow(~2.0) - 48.0 * e2 -
+                 (18.0 - 32.0 * e1 + 12.0 * e1.pow(~2.0) + 48.0 * e2 -
                   36.0 * e1 * e2 + 27.0 * e2 * e2);
         -a * b
     }
